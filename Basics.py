20da2b15fd58605f3b1ec3e858ed91f9bdca8def
oli-mac <adumulugeta21@gmail.com>
2022-08-17 13:12:32 +0300

    encoding and face detaction

--- Basics.py
+++ Basics.py
@@ -2,4 +2,22 @@ import cv2
 import numpy as np
 import face_recognition
 
+# get the encoding 
+#get our image
 
+imgElon = face_recognition.load_image_file('imagesBasic/Elon Musk.jpg')
+imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
+
+imgTest = face_recognition.load_image_file('imagesBasic/Elon Test.jpg')
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+
+#detect the face   face location printes out 4 values Top, Right ,Bottom ,Left 
+faceLoc = face_recognition.face_locations(imgElon)[0]
+#encode the face we have detacted 
+encodeElon = face_recognition.face_encodings(imgElon)[0]
+
+cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+
+cv2.imshow('Elon Musk', imgElon)
+cv2.imshow('Elon Test', imgTest)
+cv2.waitKey(0)
