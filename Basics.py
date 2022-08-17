ca1b128f1f5f8742e6115477b17e65153ba5c10d
oli-mac <adumulugeta21@gmail.com>
2022-08-17 13:30:06 +0300

    encoding and face detaction

--- Basics.py
+++ Basics.py
@@ -1,3 +1,4 @@
+from unittest import result
 import cv2
 import numpy as np
 import face_recognition
@@ -17,6 +18,22 @@ faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 
 cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
+
+# detect the face location printes out 4 values Top, Right ,Bottom ,Left 
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+#encode the face we have detacted 
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+
+cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+
+# Comparing the faces and finding the distance between them
+
+results = face_recognition.compare_faces([encodeElon],encodeTest)
+#find the distance to get the best match
+faceDist = face_recognition.face_distance([encodeElon],encodeTest)
+print(results, faceDist)
+
+#display the actual result on the image
 
 cv2.imshow('Elon Musk', imgElon)
 cv2.imshow('Elon Test', imgTest)
